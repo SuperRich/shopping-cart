27201d0295ac7330fc0cd8db7f812fbeaac3f990
SuperRich <r-hunter@live.co.uk>
2019-02-17 18:33:48 +0000

    fixed event handling
    fixed event handling

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,11 +5,21 @@ class Counters extends Component {
   state = {
     counters: [{ id: 1, value: 99 }, { id: 2, value: 0 }, { id: 3, value: 0 }]
   };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+    console.log("Test", counterId);
+  };
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} />
+          <Counter
+            key={counter.id}
+            OnDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );
